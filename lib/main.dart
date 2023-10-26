fd775918adf959d8bfffdf0c10655193dc188986
Yawn1014 <yuangao0921995@gmail.com>
2023-10-26 00:30:41 -0400

    Yippieeee a bit of UI

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,16 @@ class _MyAppState extends State {
     mapController = controller;
   }
 
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
+          title: const Text('NURGENT'),
+          backgroundColor: Color.fromARGB(255, 34, 49, 131),
+          centerTitle: true
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
@@ -34,7 +37,93 @@ class _MyAppState extends State {
             zoom: 11.0,
           ),
         ),
+        bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: SizedBox(
+          height: 56,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconBottomBar(
+                    text: "Home",
+                    icon: Icons.home,
+                    selected: _selectedIndex == 0,
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 0;
+                      });
+                    }),
+                IconBottomBar(
+                    text: "Toilets",
+                    icon: Icons.restaurant,
+                    selected: _selectedIndex == 1,
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 1;
+                      });
+                    }),
+                IconBottomBar(
+                    text: "Map",
+                    icon: Icons.map,
+                    selected: _selectedIndex == 2,
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 2;
+                      });
+                    }),
+                IconBottomBar(
+                    text: "Profile",
+                    icon: Icons.person,
+                    selected: _selectedIndex == 3,
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 3;
+                      });
+                    })
+              ],
+            ),
+          ),
+        ),
       ),
+      ),
+    );
+  }
+}
+
+class IconBottomBar extends StatelessWidget {
+  IconBottomBar(
+      {Key? key,
+      required this.text,
+      required this.icon,
+      required this.selected,
+      required this.onPressed})
+      : super(key: key);
+  final String text;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            icon,
+            color: selected ? const Color(0xff15BE77) : Colors.grey,
+          ),
+        ),
+        Text(
+          text,
+          style: TextStyle(
+              fontSize: 14,
+              height: .1,
+              color: selected ? const Color(0xff15BE77) : Colors.grey),
+        )
+      ],
     );
   }
 }
